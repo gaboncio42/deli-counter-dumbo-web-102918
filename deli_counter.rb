7705243fc841820a471ebd7c2cb29d144fbd47f6
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 15:08:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,6 +18,10 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
-  
 
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody currently waiting to be served!"
+  end
+end
 
