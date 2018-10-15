73d4cbc6fece7e8a3580e2cc2d980fa6354d8f9a
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-15 15:14:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,10 +24,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else katz_deli.length > 0
-    katz_deli.each do |name|
-      current_person.push("#{name}")
-    end
+    puts "Currently serving #{katz_deli}."
   end
-  puts "Currently serving #{current_person.join(" ")}"
 end
 
